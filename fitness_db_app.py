8883c3466b6b3c47891de8f7fc4ed5229ef5dc91
FatherofElias <supra_3_8@hotmail.com>
2024-08-27 16:46:18 -0500

    update

--- fitness_db_app.py
+++ fitness_db_app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Elias928@localhost/fitness_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:placeholder@localhost/fitness_db'
 db = SQLAlchemy(app)
 
 class Member(db.Model):
